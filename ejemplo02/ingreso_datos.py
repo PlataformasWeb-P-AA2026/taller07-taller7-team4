af75bffb2e5bd439a33e88e09503666f8b3c7fd6
Jean <jpzapata1@utpl.edu.ec>
2026-05-21 14:29:14 -0500

    finalizado

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -14,6 +14,19 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from genera_tablas import Club, Jugador
+from configuracion import cadena_base_datos
+
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
 archivo = open("data/datos_clubs.txt", "r", encoding="utf-8")
 
 for linea in archivo:
@@ -23,15 +36,13 @@ for linea in archivo:
     deporte = datos[1]
     fundacion = int(datos[2])
 
-    club_existe = session.query(Club).filter_by(nombre=nombre).first()
-    
-    if not club_existe:
-        club = Club(
-            nombre=nombre,
-            deporte=deporte,
-            fundacion=fundacion
-        )
-        session.add(club)
+    club = Club(
+        nombre=nombre,
+        deporte=deporte,
+        fundacion=fundacion
+    )
+
+    session.add(club)
 
 archivo.close()
 session.commit()
@@ -46,17 +57,16 @@ for linea in archivo:
     dorsal = int(datos[2])
     nombre = datos[3]
 
-    club = session.query(Club).filter_by(nombre=nombre_club).first()
+    club = session.query(Club).filter_by(nombre=nombre_club).one()
 
-    if club: 
-        jugador = Jugador(
-            nombre=nombre,
-            dorsal=dorsal,
-            posicion=posicion,
-            club=club
-        )
-        session.add(jugador)
+    jugador = Jugador(
+        nombre=nombre,
+        dorsal=dorsal,
+        posicion=posicion,
+        club=club
+    )
+
+    session.add(jugador)
 
 archivo.close()
 session.commit()
-
